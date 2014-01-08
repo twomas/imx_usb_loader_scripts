6c5c62120c1cf388deef0bd64b0a67ef35782e58
thomas bredhammar <thomas.bredhammar@gmail.com>
2014-01-08 00:14:12 +0000

    minor fixes

--- autostart.sh
+++ autostart.sh
@@ -2,4 +2,17 @@
 
 # Script to poll sw install
 
-sudo /home/pi/imx_usb_loader_scripts/poll_sw_install.sh &
+case "$1" in
+	start)
+		sudo /home/pi/imx_usb_loader_scripts/poll_sw_install.sh &
+		;;
+
+	stop)
+		;;
+
+	restart)
+		;;
+	*)
+		echo "Usage: $0 start|stop|restart"
+		;;
+esac
